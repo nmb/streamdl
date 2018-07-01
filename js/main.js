b1043fd75f49bf261bec33e464fe497340cc61a0
Mikael Borg <mikael.borg@nbis.se>
2018-07-01 10:39:49 +0200

    Clean-up and add some error handling.

--- js/main.js
+++ js/main.js
@@ -1,48 +1,14 @@
-//var streamSaver = require('stream');
-//var streams = require("web-streams-polyfill");
-
-//import { ReadableStream } from "web-streams-polyfill";
 var streamSaver = require('streamsaver');
 var readablestream = require('web-streams-polyfill');
 var streamBrowserify = require('stream-browserify');
 var streamMD5 = require('stream-md5/md5');
-//import { JSZip } from 'jszip/dist/jszip.js';
 var JSZip = require('jszip');
 var JSZipUtils = require('jszip-utils');
 var fs = require('fs');
-//import * as JSZipUtils from 'jszip-utils';
-//import fetchStream from 'fetch-readablestream';
-//import { createWriteStream, supported, version } from 'streamsaver'
 
 // configure streamSaver to use mitm injection on same host
 streamSaver.mitm = './mitm.html?version=' + streamSaver.version.full;
 
-function urlToPromise(url) {
-    return new Promise(function(resolve, reject) {
-        JSZipUtils.getBinaryContent(url, function (err, data) {
-            if(err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-    });
-}
-
-function handleErrors(response) {
-  if (!response.ok ) {
-    throw Error(response.statusText);
-  }
-  return response;
-}
-
-function fetchStatusHandler(response) {
-    if (response.status === 200) {
-          return response;
-            } else {
-                  throw new Error(response.statusText);
-                    }
-}
 var dlButton = document.getElementById('download-button');
 var urlinput = document.getElementById('input-box');
 console.log("Streamsaver support: " + streamSaver.supported);
@@ -51,13 +17,13 @@ function mkdl(str, z){
   let writeStream = streamSaver.createWriteStream('output.zip').getWriter();
   var lines = str.split('\n');
 
-      var readerMap = new Map();
+  var readerMap = new Map();
   for(var i = 0;i < lines.length;i++){
     // skip empty lines
     if(!lines[i].trim()) continue;
     let url = lines[i];
     // remove protocol, keep host in path
-    let filename = url.replace(/.*\/\//g, "");
+    var filename = url.replace(/.*\/\//g, "");
     try {
       new URL(url);
       console.log("Downloading " + url);
@@ -66,70 +32,58 @@ function mkdl(str, z){
       console.log("Skipping: " + url);
       continue;
     }
+    var reader
+    var state = streamMD5.init();
+    fetch(url)
+      .then(function(res) {
+        if (res.ok) {
+          return res;
+        } else {
+          throw new Error(res.statusText);
+        }
+      })
+      .then( function(res) {
+        readerMap.set(url,  new Promise((resolve, reject) => {resolve(res.body.getReader())}));
+      })
+      .catch(function(e) { 
+        console.log("error caught for " + url);
+        console.log(e);
+      });
+
     let rs = streamBrowserify.Readable()
-      let reader
-      let state = streamMD5.init();
-    readerMap.set(url, fetch(url).then(res => res.body.getReader()));
     rs._read = () => {
-      //let p = reader || (reader = fetch(url).then(res => res.body.getReader()).catch(error => console.log(error)));
-      //let p = readerMap.get(url) || (readerMap.set(url, fetch(url).then(res => res.body.getReader())));
+
       let p = readerMap.get(url);
-      p.then(reader => reader.read().then(({ value, done }) =>
+      if(!p){ rs.push(null);}
+      else {
+        p.then(reader => reader.read()
+          .then(({ value, done }) =>
             { if(done) {
-                         rs.push(null);
-                         console.log(streamMD5.finalize(state));
-                       }
-            else {
-              rs.push(new Buffer(value));
-              streamMD5.update(state, value);
+              rs.push(null);
+              console.log(streamMD5.finalize(state));
             }
-            }
-            )); //.catch(error => console.log(error) ))
-
+              else {
+                rs.push(new Buffer(value));
+                streamMD5.update(state, value);
+                console.log("pushing");
+              }
+            }))
+            .catch(error => console.log("error in read: " + error));
+      }
     }
-
-    //z.file(filename, urlToPromise("https://cors-anywhere.herokuapp.com/" + url), {binary:true});
     z.file(filename, rs);
+
   };
+
   z.generateInternalStream({type: 'uint8array', streamFiles: true})
     .on('data', data => writeStream.write(data))
     .on('error', err => console.error(err))
     .on('end', () => writeStream.close())
-  .resume()
+    .resume()
 }
 
 dlButton.addEventListener('click', function(){
   var zip = new JSZip();
-
-  /*
-zip.generateNodeStream({streamFiles:true})
-.pipe(fs.createWriteStream('out.zip'))
-.on('finish', function () {
-    // JSZip generates a readable stream with a "end" event,
-    // but is piped here in a writable stream which emits a "finish" event.
-    console.log("out.zip written.");
-});
-  */
   mkdl(urlinput.value, zip);
-
-/*  zip.generateAsync({type:"uint8array", streamFiles: true}, function updateCallback(metadata) {
-        var msg = "progression : " + metadata.percent.toFixed(2) + " %";
-        if(metadata.currentFile) {
-            msg += ", current file = " + metadata.currentFile;
-        }
-        console.log(msg);
-        //updatePercent(metadata.percent|0);
-    })
-    .then(function callback(blob) {
-
-        // see FileSaver.js
-        //saveAs(blob, "example.zip");
-
-        console.log("done !");
-    }, function (e) {
-        console.log(e);
-        console.log(e.description);
-    });
-*/
 }, false);
 
